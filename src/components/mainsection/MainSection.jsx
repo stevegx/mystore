2d5621254047d5f56a662d77f74a0f1230992401
steve vetsikas <stevevetsikas85@gmail.com>
2022-10-13 17:39:29 +0300

    Suspense added for better performance

--- src/components/mainsection/MainSection.jsx
+++ src/components/mainsection/MainSection.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, {lazy, Suspense} from 'react'
 import ProductList from './ProductList'
 import '../../styles/mainsection.css'
+
 export default function MainSection() {
   return (
+    <Suspense>
     <div className='main-section-list'>
         <ProductList/>
     </div>
+    </Suspense>
   )
 }
