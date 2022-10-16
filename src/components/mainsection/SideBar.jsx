ebd518532851f166cf0e191c81c7632ec448d2dd
steve vetsikas <stevevetsikas85@gmail.com>
2022-10-16 10:16:35 +0300

    fixed searchfilter

--- src/components/mainsection/SideBar.jsx
+++ src/components/mainsection/SideBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState , useContext, useEffect} from 'react'
+import React, { useState , useContext, useEffect,useRef} from 'react'
 import '../../styles/sidebar.css'
 import StoreContext from '../../StoreContext'
 import { isAccordionItemSelected } from 'react-bootstrap/esm/AccordionContext'
@@ -8,7 +8,6 @@ export default function SideBar() {
     const [min,setMin] = useState(0)
     const [max,setMax] = useState(1100)
     const [priceValue,setPriceValue] = useState(0)
-    const [testArray,setTestArray] = useState([])
     const [category,setCategory] = useState([
     {index:0,id:"men's clothing" , isChecked:false},
     {index:1,id:"jewelery" , isChecked:false},
@@ -16,48 +15,46 @@ export default function SideBar() {
     {index:3,id:"women's clothing" , isChecked:false},
   ])
 
-    useEffect(()=>{
-        let categoryArray = category.filter(obj => obj.isChecked == true)
-        let categoryFilter = []
-        for(let i=0;i<categoryArray.length;i++){
-        categoryFilter[i]=item.products.filter(product => product.category == categoryArray[i].id)
-        }   
-        if(categoryFilter.length>0)
-        {
-            for(let i=0;i<categoryFilter.length;i++)
-            {
-                item.setProducts(categoryFilter[i]) //prepei na breis tropo na sundiaseis pollapla input categories gt twra pairnei mono to ena
-                
-            }
-        }else{
-            item.setProducts(item.getProducts)
-        }
-        // dokimase setCount(currentCount =>{return currentCount + amount})
-        // console.log(categoryFilter);
-        console.log(item.products);
-        // setTestArray(categoryFilter)
-       
-     
-        let newArray = item.products.filter(product => product.price > priceValue)
-        console.log(newArray);
-        
-        // item.setProducts(newArray)
- 
-    },[priceValue,category])
+
+useEffect(()=>{
     
-  const handleCheck = (e)=>{
+    let categoryFilter = []
+    for (let index = 0; index < category.length; index++) {
+       if(category[index].isChecked ==true){
+           let value = item.getProducts.filter(product => product.category == category[index].id)
+            categoryFilter=[...categoryFilter,value]
+       }
+        }   
+
+    let example = categoryFilter.flat(1)
+    let newArray
    
+ 
+    if(example.length==0)
+    {
+        newArray = item.products
+        item.setProducts(item.getProducts)
+        newArray = item.getProducts.filter(product => product.price > priceValue)
+        item.setProducts(newArray)
+    }else if(example.length!=0){
+       newArray =  example.filter(product=>product.price > priceValue)
+        item.setProducts(newArray)
+    }
+    // sxedon eimaste konta prepei na breis otan kanena filtro apo to kategory den einai checked h to prica an einai mhden na kanei reset
+},[category,priceValue])
+
+const handleCheck = (e)=>{
     setCategory(category.map(obj =>{
+
         if(obj.index == e.target.value){
             return {...obj, isChecked:!obj.isChecked}
         }else{
                 return obj
             }
     }))
-//  map to category k an einai checked tote pane sto products k ftiakse ena kainourgio array me filter 
-    
-    }
-
+}
+ 
+  
   
   return (
     <div className='sidebar-container'>
@@ -67,7 +64,7 @@ export default function SideBar() {
             <h3>Category <span className='product-array-length'>({item.products.length})</span></h3>
             <div className='sidebar-category-option'>
                 <label htmlFor="mensclothing">Men's clothing</label>
-                <input type="checkbox" className='mensclothing checkbox' id='mensclothing' onChange={handleCheck} value={category[0].index}/>
+                <input type="checkbox" className='mensclothing checkbox'  id='mensclothing' onChange={handleCheck} value={category[0].index}/>
             </div>
             <div className='sidebar-category-option'>
                 <label htmlFor="Jewelery">Jewelery</label>
